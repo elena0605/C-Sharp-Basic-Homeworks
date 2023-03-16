32d8ffc2063c7bc552f26c4f1c357f8679ef5189
elena0605 <ecka.0605@gmail.com>
2023-03-16 15:52:55 +0100

    homework tasks

---
+++ Class2Homework/AverageNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create new console application “AverageNumber” that takes four numbers as input to calculate and print the average.
+//Test Data:
+//Enter the First number: 10
+//Enter the Second number: 15
+//Enter the third number: 20
+//Enter the four number: 30
+//Expected Output:
+//The average of 10, 15, 20 and 30 is: 18
+
+Console.WriteLine("Enter the first number: ");
+double firstNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+double secondNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third number: ");
+double thirdNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the fourth number: ");
+double fourthNumber = double.Parse(Console.ReadLine());
+int numberOfInputCounts = 4;
+double averageResult = (firstNumber + secondNumber + thirdNumber + fourthNumber)/numberOfInputCounts;
+Console.WriteLine($"The average of {firstNumber}, {secondNumber}, {thirdNumber} and {fourthNumber} is: {averageResult}");
---
+++ Class2Homework/RealCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿
+//Create new console application “RealCalculator” that takes two numbers from the input and asks what operation would the user want to be done ( +, - , * , / ). Then it returns the result.
+//Test Data:
+//Enter the First number: 10
+//Enter the Second number: 15
+//Enter the Operation: +
+//Expected Output:
+//The result is: 25
+
+Console.WriteLine("Enter the first number: ");
+double firstNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+double secondNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the calculation operator:");
+bool ifParsingSucceeded = char.TryParse(Console.ReadLine(), out char calcOperator);
+
+if (ifParsingSucceeded)
+{
+    double result;
+    switch (calcOperator) {
+
+        case '+':
+            result = firstNumber + secondNumber; 
+            Console.WriteLine($"The result of the Sum operation is {result}");
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            Console.WriteLine($"The result of the Subtraction operation is {result}");
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            Console.WriteLine($"The result of the Multiplication operation is {result}");
+            break;  
+        case '/':
+            result = firstNumber / secondNumber;
+            Console.WriteLine($"The result of the Division operation is {result} ");
+            break;
+        default:
+            Console.WriteLine("You have entered wrong operator");
+            break;
+             
+    }
+
+}
+
+
